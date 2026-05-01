c460a38d52acd4e92a6116c007ba39ddb2308376
Diana Mircea <mirceadiana15@gmail.com>
2026-05-01 11:20:43 -0600

    finally fixed issues

--- json.go
+++ json.go
@@ -33,5 +33,4 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	if _, err := w.Write(dat); err != nil {
 		log.Printf("failed to write response")
 	}
-	w.Write(dat)
 }
--- main.go
+++ main.go
@@ -87,17 +87,19 @@ func main() {
 	}
 
 	v1Router.Get("/healthz", handlerReadiness)
-
 	router.Mount("/v1", v1Router)
-	srv := &http.Server{
-		Addr:              ":" + port,
-		Handler:           router,
-		ReadHeaderTimeout: 10 * time.Second,
+	
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+	    log.Fatalf("Invalid port: %s", err)
 	}
 
-	if _, err := strconv.Atoi(port); err != nil {
-		log.Fatalf("Invalid port: %s", err)
+	srv := &http.Server{
+	    Addr:              ":" + port,
+	    Handler:           router,
+	    ReadHeaderTimeout: 10 * time.Second,
 	}
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+
+	log.Printf("Serving on port: %d\n", portNum)
+	log.Fatal(srv.ListenAndServe())	
 }
